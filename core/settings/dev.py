e3e06af4edeb94f2d546c074ac61c59fc58c772f
Mahamed Ali <98144685+mahamedbinali@users.noreply.github.com>
2024-09-27 17:34:07 +0300

    Project settings and added shop app

---
+++ core/settings/dev.py
@@ -0,0 +1,30 @@
+from decouple import config
+
+from .base import *
+
+SECRET_KEY = config("SECRET_KEY")
+
+DEBUG = config("DEBUG", default=True, cast=bool)
+
+ALLOWED_HOSTS = config("ALLOWED_HOSTS").split(",")
+
+USE_SQLITE = config("USE_SQLITE", default=True, cast=bool)
+
+if USE_SQLITE:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": config("DB_ENGINE"),
+            "NAME": config("DB_NAME"),
+            "USER": config("DB_USER"),
+            "PASSWORD": config("DB_PASSWORD"),
+            "HOST": config("DB_HOST"),
+            "PORT": config("DB_PORT"),
+        },
+    }
